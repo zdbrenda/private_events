bcbd5c7798e7bb1ececdee144b4f363c4c3d528c
zdbrenda <zdbrenda@gmail.com>
2021-11-14 14:41:10 -0700

    Add events controller and related form to let users create new events, and display created events by that user from user_page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,12 +3,22 @@ class EventsController < ApplicationController
     end
 
     def show
+        @event = Event.find(params[:id])
     end
 
     def new
+        @event = current_user.created_events.build
     end
 
     def create
+        @event = current_user.created_events.build(event_params)
+        respond_to do |format|
+            if @event.save
+                format.html { redirect_to user_path(current_user.id), notice: "Event was successfully created."}
+            else
+                format.html { render :new }
+            end
+        end
     end
 
     def edit
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root to: "events#index"
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/users/:id', to:"users#show", as: "user"
+  resources :events
   
 end
