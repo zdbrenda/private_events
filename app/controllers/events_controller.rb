897cb523ce2995d495b7e070735b6a586531ebb5
zdbrenda <zdbrenda@gmail.com>
2021-11-15 15:53:13 -0700

    Allow users to delete/edit events they created, add some more styling

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -23,12 +23,25 @@ class EventsController < ApplicationController
     end
 
     def edit
+        @event=Event.find(params[:id])
+        
     end
 
     def update
+        @event = Event.find(params[:id])
+        
+        if @event.update(event_params)
+            redirect_to @event
+        else
+            render :edit
+        end
     end
 
     def destroy
+        @event = Event.find(params[:id])
+        @event.destroy
+        
+        redirect_to user_path(current_user.id)
     end
 
     private
