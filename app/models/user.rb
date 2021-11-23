451cb72f9f110d966b8acbf2c746c4f5d82d04a9
zdbrenda <zdbrenda@gmail.com>
2021-11-23 16:15:55 -0700

    Add dependent destroy for user model so that user can destroy their account

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :created_events, class_name: "Event", foreign_key: "creator_id"
-  has_many :attending_events
+  has_many :created_events, class_name: "Event", foreign_key: "creator_id", dependent: :destroy
+  has_many :attending_events, dependent: :destroy
   has_many :attended_events, through: :attending_events, source: :event,  foreign_key: "attendee_id"
 end
