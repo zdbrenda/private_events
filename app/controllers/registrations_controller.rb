b92ff9017fcef0a23d9ba1037704c629bb6cc70a
zdbrenda <zdbrenda@gmail.com>
2021-11-23 15:14:50 -0700

    Allow devise edit account without password

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,12 @@
 class RegistrationsController < Devise::RegistrationsController
+
+    protected
+    def update_resource(resource, params)
+        return super if params["password"]&.present?
+
+        resource.update_without_password(params.except("current_password"))
+    end
+
     private
 
     def sign_up_params
